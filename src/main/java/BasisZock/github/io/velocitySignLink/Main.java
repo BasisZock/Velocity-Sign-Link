df9f68b694300a7bd59a4e6b387b68cc1caa200e
BasisZock <kekslecker2000@gmx.de>
2025-04-03 19:14:28 +0200

    Include bStats

--- src/main/java/BasisZock/github/io/velocitySignLink/Main.java
+++ src/main/java/BasisZock/github/io/velocitySignLink/Main.java
@@ -7,11 +7,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import org.bstats.bukkit.Metrics;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        //Enable bStats
+        int pluginId = 25343;
+        Metrics metrics = new Metrics(this, pluginId);
         // Initialize database and configuration
         DatabaseManager.createDatabase();
         DatabaseManager.connect();
