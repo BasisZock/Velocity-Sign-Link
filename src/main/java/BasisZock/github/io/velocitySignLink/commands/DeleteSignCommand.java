5fd2f4e8394bbca40c6ed4dcaa80f253ad82b293
BasisZock <kekslecker2000@gmx.de>
2025-06-03 22:00:56 +0200

    Initial upload for the new DeleteSign command file

---
+++ src/main/java/BasisZock/github/io/velocitySignLink/commands/DeleteSignCommand.java
@@ -0,0 +1,14 @@
+package BasisZock.github.io.velocitySignLink.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
+
+public class DeleteSignCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        return false;
+    }
+}
